6c7fce47d4ac9a2f4ed77f9ce31514e5d8688fa1
Von-Onion <rumoto126@gmail.com>
2024-02-14 14:09:09 -0500

    Other

---
+++ Platform_Santos/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows.Speech;
+
+public class GameManager : MonoBehaviour
+{
+    public float timeValue = 1;
+    public Rigidbody2D rb2d;
+    public float currentGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Platform_Santos/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    private float xMovement = 0f;
+
+    public float speed = 40f;
+
+    private bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(xMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
