58171f7dedb22e161384e19184cb05edcf334d56
Von-Onion <rumoto126@gmail.com>
2024-02-28 08:22:28 -0500

    Other

--- Platform_Santos/Assets/Scripts/Button.cs
+++ Platform_Santos/Assets/Scripts/Button.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    [Header("Objects")]
+
     [SerializeField] private GameObject door;
     [SerializeField] private GameObject target;
     [SerializeField] private GameObject doorTarget;
+
+    [Header("Values")]
+    [SerializeField] private float buttonTime = 1f;
+    [SerializeField] private float doorTime = 1f;
 
     private bool isPressed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPressed){
+            transform.position = Vector2.Lerp(transform.position, target.transform.position, Time.deltaTime * buttonTime);
+            door.transform.position = Vector2.Lerp(door.transform.position, doorTarget.transform.position, Time.deltaTime * doorTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        transform.position = target.transform.position;
-        door.transform.position = doorTarget.transform.position;
+       isPressed = true;
     }
 }
