03c6f82acc5fdb359a242255ac221e7f97c7c6a8
Von-Onion <rumoto126@gmail.com>
2026-05-23 11:34:07 -0400

    Work on gravity flipping

--- Platform_Santos/Assets/Scripts/Other/GravityFlip.cs
+++ Platform_Santos/Assets/Scripts/Other/GravityFlip.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class GravityFlip : MonoBehaviour
 {
-    public float smoothing = 1.0f;
+    public Transform from;
+    public Transform to;
+    private bool isGravityFlipped = false;
+    private float timeCount = 0.0f;
+    [SerializeField] private float speed = 1.5f;
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.F)){
+            if(!isGravityFlipped){
+             timeCount = 0f;
+            isGravityFlipped = true;
+            Physics2D.gravity *= -1;
+        } else {
+            timeCount = 0f;
+            isGravityFlipped = false;
+            Physics2D.gravity *= -1;
+        }
+        }
+        
+
         
     }
+    
     private void OnTriggerEnter2D(Collider2D other) {
         Physics2D.gravity *= -1;
-        this.transform.Rotate = (0f, 0f, 90f);
+        if(!isGravityFlipped){
+            timeCount = 0f;
+            isGravityFlipped = true;
+        } else {
+            timeCount = 0f;
+            isGravityFlipped = false;
+        }
+        if(!isGravityFlipped){
+            transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount + Time.deltaTime);
+        } else{
+            transform.rotation = Quaternion.Slerp(to.rotation, from.rotation, timeCount + Time.deltaTime);
+        }
+
     }
 }
